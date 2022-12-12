6b6d6888e0a823761855ed99ab4ca7316f4e201e
Amit <yorsamit1@gmail.com>
2022-12-12 15:20:28 +0530

    Updated Api Base url

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export let BASE_URL = "https://tgubackend.herokuapp.com/"
+export let BASE_URL = "https://tgbackend-i7w7.onrender.com/"
 // export let BASE_URL = "http://localhost:4000/"
 
 let options:any = {
